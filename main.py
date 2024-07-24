7bcac4f69a356ab42a6c36c03a3e0be2dd7020cf
v <v@mail.ru>
2024-07-24 22:37:07 +0500

    feat: Написал базовую версию скрипта

--- main.py
+++ main.py
@@ -6,7 +6,32 @@
 Через библиотеку requests и beautifulsoup
 # pip install requests
 # pip install beautifulsoup4
+
+Скриптовый стиль!
 """
 
+from re import X
 import requests
 from bs4 import BeautifulSoup
+
+
+URL = 'https://www.gamesvoice.ru/swsurvivor'
+XPATH = '//*[@id="comp-lh1g6uej"]/h1/span/span/span/span'
+
+# Делаем GET запрос на сайт
+response = requests.get(URL)
+
+# Проверяем статус ответа
+if response.status_code == 200:
+    print('Успех. Сайт доступен')
+else:
+    print('Ошибка. Сайт недоступен')
+    exit()
+
+# Анализируем полученный ответ, с помощью парсера, ищем элемент //*[@id="comp-lh1g6uej"]/h1/span/span/span/span
+html = BeautifulSoup(response.text, 'html.parser')
+
+# Проверяем наличие текста "Этот русификатор пока ещё недоступен" в элементе ХPATH
+if html.select_one(XPATH).text == 'Этот русификатор пока ещё недоступен':
+    print('Русификатор еще не доступен')
+
